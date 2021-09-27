cf2c18ed2f8c54b5e409a4eb0aebd2a9c4c6e2e0
Trevor Martin <trevor.j.martin@gmail.com>
2021-09-26 22:36:39 -0400

    update client menu

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,10 @@ import {
   useHistory,
   Link,
 } from "react-router-dom";
-import { Button, Box, Menu, MenuItem } from "@mui/material";
+import { Button, Box, Menu, MenuItem, Divider } from "@mui/material";
+import YardIcon from "@mui/icons-material/Yard";
+import SmokeFreeIcon from "@mui/icons-material/SmokeFree";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import {
   usePopupState,
   bindTrigger,
@@ -56,6 +59,19 @@ function App() {
     console.log(result);
     setState({ ...state, refresh: true, newProduct: productSchema });
   };
+
+  const newProductProps = {
+    openForm,
+    setOpenForm,
+    newProduct: state.newProduct,
+    handleSubmit,
+    handleChange: (e) =>
+      setState({
+        ...state,
+        newProduct: { ...state.newProduct, [e.target.name]: e.target.value },
+      }),
+  };
+
   const menuClicked = (e) => {
     setState({ ...state, format: e.target.id });
     popupState.close();
@@ -76,13 +92,44 @@ function App() {
           <Button variant="outlined" {...bindTrigger(popupState)}>
             {state.title}
           </Button>
-          <Menu {...bindMenu(popupState)}>
+          <Menu
+            {...bindMenu(popupState)}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                mt: 1.5,
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
+                },
+                "&:before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
             <MenuItem
               id="flower"
               component={Link}
               to={"/products/flower"}
               onClick={menuClicked}
             >
+            <YardIcon sx={{marginRight: "1rem"}}/>
               Flower
             </MenuItem>
             <MenuItem
@@ -91,7 +138,18 @@ function App() {
               to="/products/oil"
               onClick={menuClicked}
             >
+            <SmokeFreeIcon sx={{marginRight: "1rem"}}/>
               Oil
+            </MenuItem>
+            <Divider variant="middle" />
+            <MenuItem
+              id=""
+              component={Link}
+              to="/products/admin"
+              onClick={menuClicked}
+            >
+              <AdminPanelSettingsIcon sx={{marginRight: "1rem"}} />
+              Admin Panel
             </MenuItem>
           </Menu>
         </Box>
@@ -110,22 +168,9 @@ function App() {
                 New Product
               </Button>
             </Box>
+            <Divider variant="fullWidth" />
             <div className="product-form-containter">
-              <NewProduct
-                openForm={openForm}
-                setOpenForm={setOpenForm}
-                newProduct={state.newProduct}
-                handleSubmit={handleSubmit}
-                handleChange={(e) =>
-                  setState({
-                    ...state,
-                    newProduct: {
-                      ...state.newProduct,
-                      [e.target.name]: e.target.value,
-                    },
-                  })
-                }
-              />
+              <NewProduct {...newProductProps} />
             </div>
             <ProductList
               {...defaultProps}
@@ -134,14 +179,28 @@ function App() {
             />
           </Route>
           <Route path="/products/flower">
+            <Divider variant="fullWidth" />
             <ProductList {...defaultProps} format="flower" title="Flower" />
           </Route>
           <Route path="/products/oil">
+            <Divider variant="fullWidth" />
             <ProductList {...defaultProps} format="oil" title="Oil" />
           </Route>
           <Route path="/">
-            <h1>Home</h1>
-            <img src="/mj.jpg" alt="marijuana leaf" />
+            <Box
+              component="img"
+              src="/mj.jpg"
+              alt="marijuana leaf"
+              sx={{
+                borderRadius: 1,
+                mx: "auto",
+                p: 2,
+                m: 1,
+                width: "80vw",
+                maxWidth: "42rem",
+                textAlign: "center",
+              }}
+            />
           </Route>
         </Switch>
       </div>
